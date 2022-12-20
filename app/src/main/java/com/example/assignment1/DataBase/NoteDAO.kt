a06f563a212d4af1393c0f5f6c8a1d5b644e62db
akshayv11git <120934789+akshayv11git@users.noreply.github.com>
2022-12-20 11:05:01 +0530

    done with FE

---
+++ app/src/main/java/com/example/assignment1/DataBase/NoteDAO.kt
@@ -0,0 +1,20 @@
+package com.example.assignment1.DataBase
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.assignment1.Models.Note
+
+@Dao
+interface NoteDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note:Note)
+    @Query("Select * from notes_table order by id ASC")
+    fun getAllNote():LiveData<List<Note>>
+
+   // suspend fun update(id :Int?,title:String?,note:String?)
+    @Query("UPDATE notes_table Set title =:title,note=:note WHERE id=:id")
+            suspend fun update(id:Int?,title:String?,note: String?)
+}
